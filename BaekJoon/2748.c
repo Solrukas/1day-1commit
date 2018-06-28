aa03f30de7c6c4f5e72c1679f203dafc6c31b1f5
Solrukas <wlsdn23167@naver.com>
2018-06-28 19:41:32 +0900

    Update 2748.c
    BaekJoon No.2748
    Code by C

--- BaekJoon/2748.c
+++ BaekJoon/2748.c
@@ -3,14 +3,15 @@
 int main() {
     int num;
     long long int al[91];
+    long long int 
     al[0] = 0;
     al[1] = 1;
  
     scanf("%d", &num);
  
     if (num >= 2) {
-        for (int a = 2; a <= num; a++) {
-            al[a] = al[a - 1] + al[a - 2];
+        for (int i = 2; i <= num; i++) {
+            al[i] = al[i - 1] + al[i - 2];
         }
     }
  
