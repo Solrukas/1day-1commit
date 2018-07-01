c610a45142eef18d0adb877b169ac076815362e7
Solrukas <wlsdn23167@naver.com>
2018-07-01 21:07:38 +0900

    Update 2749.c
    BaekJoon No.2749
    Code by C

--- BaekJoon/2749.c
+++ BaekJoon/2749.c
@@ -3,7 +3,9 @@
 int main(){
 	long long num, st = 0, nd = 1, rd = st + nd;
 	scanf("%lld", &num);
+	
 	for(num = num % (1500000); num --> 1; st = nd % 1000000, nd = rd % 1000000)
 		rd = (st + nd) % 1000000;
+	
 	printf("%lld",rd);
 }
