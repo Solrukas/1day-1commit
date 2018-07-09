9ec1a64c862b50ab63ebd4ae85d14785a733e823
Solrukas <wlsdn23167@naver.com>
2018-07-09 21:56:52 +0900

    Create 2438.c
    BaekJoon No.2438
    Code by C

---
+++ BaekJoon/2438.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main() {
+	int num;
+	scanf("%d", &num);
+	
+	for (int i = 0; i < num; i++) {
+		for (int j = 0; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
