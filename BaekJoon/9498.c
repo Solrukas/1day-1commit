91d9210f62625c07603dd6d3b3422570de93821e
Solrukas <wlsdn23167@naver.com>
2018-07-18 21:26:35 +0900

    Create 9498.c
    BaekJoon No.9498
    Code by C

---
+++ BaekJoon/9498.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main() {
+	int num;
+	scanf("%d", &num);
+
+	if (num >= 90) {
+		printf("A");
+	}
+	else if (num >= 80) {
+		printf("B");
+	}
+	else if (num >= 70) {
+		printf("C");
+	}
+	else if (num >= 60) {
+		printf("D");
+	}
+	else {
+		printf("F");
+	}
+}
