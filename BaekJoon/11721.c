30e2522b070f5e1e55eeaa9e6a52dc9497f4e111
Solrukas <wlsdn23167@naver.com>
2018-07-24 22:31:35 +0900

    Update 11721.c
    BaekJoon No.11721
    Code by C

--- BaekJoon/11721.c
+++ BaekJoon/11721.c
@@ -5,10 +5,10 @@ int main()
 {
 	char str[100];
 	int i = 0, len = 0;
-
+	
 	scanf("%s", str);
-
 	len = strlen(str);
+	
 	for (i = 0; i < len; i++) {
 		printf("%c", str[i]);
 		if ((i + 1) % 10 == 0)
