ab39a98055b68c3931758c7a386c2e7f924ff714
Solrukas <wlsdn23167@naver.com>
2018-07-25 22:37:11 +0900

    Update 10871.c

--- BaekJoon/10871.c
+++ BaekJoon/10871.c
@@ -2,17 +2,17 @@
 #pragma warning(disable:4996)
 
 int main() {
-	int num, a, b, i, count = 0, arr[10000] = { 0, };
+	int num, a, b, i, cnt = 0, arr[10000] = { 0, };
 	scanf("%d %d", &num, &a);
 
 	for (i = 0; i < num; i++) {
 		scanf("%d", &b);
 		if (b < a) {
-			arr[count] = b;
-			count++;
+			arr[cnt] = b;
+			cnt++;
 		}
 	}
-	for (i = 0; i < count; i++) {
+	for (i = 0; i < cnt; i++) {
 		printf("%d ", arr[i]);
 	}
 }
